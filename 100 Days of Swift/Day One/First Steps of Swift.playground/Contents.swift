739a55014f14cdd82faa44cb34214868cdc87425
Michele Pipicelli <ava@avaisgay.me>
2021-06-03 10:55:37 +0930

    first commit

---
+++ 100 Days of Swift/Day One/First Steps of Swift.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+
+var str = "Some string";
+str = "Playground";
+
+var age = 38; // Stores integer
+var population = 8_000_000;
+
+// Standard swift strings can't use line breaks
+// Multi-Line Strings need 3 """
+
+var someString = """
+This goes
+Over Multiple
+Line Breaks
+""";
+
+//print(string)
+
+
+
+var pi = 3.141 // Double
+var awesome = false;
+
+// String Interpolation
+// Place vars inside strings
+
+var example = "This is an example \(awesome)"
+
+// Constant
+
+let i = 0
+// i = 5 - Exception
+
+var test = 1_000_000_000;
+print(test);
+
+// Type Annotations
+
+let string: String = "Playground";
+let int: Int = 5123;
+let boolean: Bool = false;
+
+print(string);
+print(int);
+print(boolean)
